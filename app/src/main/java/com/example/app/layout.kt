fc0b876d6f8aa53172e69cbcb6338c8ba124106f
taznimt <taznimanwar06@gmail.com>
2025-10-16 15:03:38 +0700

    Menambahkan baris pertama dengan tiga komponen Text pada ColumnRow

--- app/src/main/java/com/example/app/layout.kt
+++ app/src/main/java/com/example/app/layout.kt
@@ -63,6 +63,15 @@ fun TataletakBox(modifier: Modifier) {
 @Composable
 fun TataletakColumnRow(modifier: Modifier){
     Column(){
+        //BAris1
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+
+        }
+
 
     }
 }
