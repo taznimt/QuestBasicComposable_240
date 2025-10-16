60b7e52086a978d221a076367e2556fbfe59b232
taznimt <taznimanwar06@gmail.com>
2025-10-16 15:05:34 +0700

    complete TataletakColumnRow with two Row layouts

--- app/src/main/java/com/example/app/layout.kt
+++ app/src/main/java/com/example/app/layout.kt
@@ -71,6 +71,13 @@ fun TataletakColumnRow(modifier: Modifier){
             Text(text = "Komponen3Baris1")
 
         }
+        //Baris2
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponen2Baris2")
+            Text(text = "Komponen3Baris2")
+        }
 
 
     }
